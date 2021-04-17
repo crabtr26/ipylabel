625746ae4cd1f8749e3a59569cdc4ff1b99c968d
Jacob Crabtree <crabtr26@gmail.com>
2021-04-17 00:31:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,6 @@ setup(
     install_requires=requirements,
     keywords='ipylabel',
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
