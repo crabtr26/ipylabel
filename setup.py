e36b2d821db240edd8cf58b48acfadf39cf0d11f
crabtr26 <crabtr26@gmail.com>
2021-04-17 00:27:40 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import versioneer
+
+requirements = [
+    # package requirements go here
+]
+
+setup(
+    name='ipylabel',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="[An ipywidgets dashboard library for converting raw data sources into ML ready datasets.]",
+    license="MIT",
+    author="Jacob Crabtree",
+    author_email='crabtr26@gmail.com',
+    url='https://github.com/crabtr26/ipylabel',
+    packages=['ipylabel'],
+    
+    install_requires=requirements,
+    keywords='ipylabel',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
